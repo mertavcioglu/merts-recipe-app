33c6565d570e4c2d1e1d3fb6b4b2a835d2cd190d
Mert Avcioglu <mavciog1@binghamton.edu>
2020-06-12 12:52:54 +0300

    git problem

---
+++ README.md
@@ -0,0 +1,11 @@
+# recipe-app
+
+https://merts-recipe-app.herokuapp.com/
+
+This is my Recipe App 🍔
+
+It uses the API from: https://www.edamam.com 
+
+Here is a screenshot of the page:
+
+![screenshot](https://user-images.githubusercontent.com/34686131/84485402-c51f5380-aca4-11ea-88f6-cfc569c33aeb.png)
---
+++ src/components/Form.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Form(props) {
+
+  return (
+
+    <form onSubmit={props.getSearch} className="search-form">
+      <input
+        autoFocus
+        className="search-bar"
+        onChange={props.onChange}
+        type="text"
+        value={props.search}
+        placeholder="Search for a food..."
+      />
+      <button className="btn btn-danger" type="submit">Search</button>
+    </form>
+
+  )
+}
+
+export default Form
